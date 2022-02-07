0c43ce9116f2b4522f03ab14a857241b2accec85
gabriel-lamarao <gabriel.lamarao97@gmail.com>
2022-02-07 12:55:52 -0300

    ParkingSpotService created

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,2 +1,7 @@
-package com.api.parkingcontrol.services;public class ParkingSpotService {
+package com.api.parkingcontrol.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
 }

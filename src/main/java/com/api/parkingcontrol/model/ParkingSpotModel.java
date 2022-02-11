3eff0488666aa8e86a58c24190c662d8bf658bb5
gabriel-lamarao <gabriel.lamarao97@gmail.com>
2022-02-11 11:13:38 -0300

    ParkingSpotModel Fix replacing TB_PARKING-SPOT > TB_PARKING_SPOT

--- src/main/java/com/api/parkingcontrol/model/ParkingSpotModel.java
+++ src/main/java/com/api/parkingcontrol/model/ParkingSpotModel.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 
 @Entity
-@Table(name = "TB_PARKING-SPOT")
+@Table(name = "TB_PARKING_SPOT")
 public class ParkingSpotModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
